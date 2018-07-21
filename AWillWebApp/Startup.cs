c2f0db077aac245c8974197d4e46fed4bc65d7ac
Anthony Williams <anthony.williams.cs@gmail.com>
2018-07-21 10:14:03 -0600

    Enabled MVC, added default view

--- AWillWebApp/Startup.cs
+++ AWillWebApp/Startup.cs
@@ -11,6 +11,7 @@ namespace AWillWebApp
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddMvc();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -21,10 +22,13 @@ namespace AWillWebApp
 				app.UseDeveloperExceptionPage();
 			}
 
-			app.Run(async (context) =>
-			{
-				await context.Response.WriteAsync("Hello World!");
-			});
+			app.UseStaticFiles();
+
+			app.UseMvc(routes =>
+				{
+					routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+				}
+			);
 		}
 	}
 }
