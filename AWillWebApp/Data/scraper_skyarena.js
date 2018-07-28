2429c704a05098d88150aa54a0fb88d1e9c76807
Anthony Williams <anthony.williams.cs@gmail.com>
2018-07-28 17:15:41 -0600

    First pass at monster scraping from skyarena

---
+++ AWillWebApp/Data/scraper_skyarena.js
@@ -0,0 +1,34 @@
+﻿/*
+ * Author: Anthony Williams
+ * Version: Alexandra
+ * Date: 07/28/2018
+ * Target Site: summonerswarskyarena.info
+ * Target URL: https://summonerswarskyarena.info/monster-list/
+ * Purpose: Grab all tr elements on page and parse them to create basic data model objects
+ */
+
+const DOMRowToMonster = (rowDOM) => {
+	const nameElements = rowDOM.getElementsByClassName('name')
+	const awakenedName = nameElements[1].textContent
+
+	const currentAnchor = nameElements[0].getElementsByTagName('a')[0]
+	const element = currentAnchor.getElementsByClassName('element')[0].textContent
+	const name = currentAnchor.getElementsByTagName('h3')[0].textContent
+
+	return {
+		awakenedName,
+		element,
+		name
+	}
+}
+
+const parseMonsters = () => {
+	const allMonsterRows = $('tr.searchable')
+	const generatedMonsters = []
+
+	for (let index = 0; index < allMonsterRows.length; index++) {
+		generatedMonsters.push(DOMRowToMonster(allMonsterRows[index]))
+	}
+
+	return generatedMonsters
+}
