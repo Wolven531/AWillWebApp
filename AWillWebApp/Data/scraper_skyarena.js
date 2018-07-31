bebc8262efb2fa65ce18983c2192952e869e45e8
Anthony Williams <anthony.williams.cs@gmail.com>
2018-07-30 21:52:53 -0600

    Updated skyarena scraper to grab more data

--- AWillWebApp/Data/scraper_skyarena.js
+++ AWillWebApp/Data/scraper_skyarena.js
@@ -1,24 +1,59 @@
 ﻿/*
  * Author: Anthony Williams
- * Version: Alexandra
- * Date: 07/28/2018
+ * Version: Baretta
+ * Date: 07/30/2018
  * Target Site: summonerswarskyarena.info
  * Target URL: https://summonerswarskyarena.info/monster-list/
  * Purpose: Grab all tr elements on page and parse them to create basic data model objects
  */
 
+const DOMElementsToRuneInfo = (runesDOMElements) => {
+	let runeList = ''
+	let runeValues = ''
+
+	if (runesDOMElements && runesDOMElements.length > 0) {
+		const runesDOMElement = runesDOMElements[0]
+		const runeListDOMElements = runesDOMElement.getElementsByClassName('rune-list')
+		const runeValuesDOMElements = runesDOMElement.getElementsByClassName('rune-values')
+
+		if (runeListDOMElements && runeListDOMElements.length > 0) {
+			runeList = runeListDOMElements[0].textContent
+		}
+
+		if (runeValuesDOMElements && runeValuesDOMElements.length > 0) {
+			runeValues = runeValuesDOMElements[0].textContent
+		}
+	}
+
+	return {
+		runeList,
+		runeValues
+	}
+}
+
 const DOMRowToMonster = (rowDOM) => {
 	const nameElements = rowDOM.getElementsByClassName('name')
+	const rating = parseInt(rowDOM.getElementsByClassName('rating')[0].textContent, 10)
+	const statPriority = rowDOM.getElementsByClassName('stat-priority')[0].textContent
 	const awakenedName = nameElements[1].textContent
 
 	const currentAnchor = nameElements[0].getElementsByTagName('a')[0]
 	const element = currentAnchor.getElementsByClassName('element')[0].textContent
 	const name = currentAnchor.getElementsByTagName('h3')[0].textContent
 
+	const earlyRuneInfo = DOMElementsToRuneInfo(rowDOM.getElementsByClassName('early-runes'))
+	const lateRuneInfo = DOMElementsToRuneInfo(rowDOM.getElementsByClassName('late-runes'))
+
 	return {
 		awakenedName,
+		earlyRuneList: earlyRuneInfo.runeList,
+		earlyRuneValues: earlyRuneInfo.runeValues,
 		element,
-		name
+		lateRuneList: lateRuneInfo.runeList,
+		lateRuneValues: lateRuneInfo.runeValues,
+		name,
+		rating,
+		statPriority
 	}
 }
 
@@ -32,3 +67,5 @@ const parseMonsters = () => {
 
 	return generatedMonsters
 }
+
+const monsters = parseMonsters()
