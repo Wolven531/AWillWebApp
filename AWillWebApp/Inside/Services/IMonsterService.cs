ac0976e082e6be176a4e7cdbb2e2df562d491d67
Anthony Williams <anthony.williams.cs@gmail.com>
2018-07-31 18:19:02 -0600

    Added initial MonsterService interface

---
+++ AWillWebApp/Inside/Services/IMonsterService.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="IMonsterService.cs" company="AWill Inc">
+// Copyright (c) 2018 AWill Inc; All rights reserved.
+// </copyright>
+
+namespace AWillWebApp.Inside.Services
+{
+	//using System;
+	using System.Collections.Generic;
+	using System.Threading.Tasks;
+	//using AWillWebApp.Inside.Models;
+
+	public interface IMonsterService
+	{
+		Task<IEnumerable<string>> SearchMonsterNames(string searchQuery);
+	}
+}
