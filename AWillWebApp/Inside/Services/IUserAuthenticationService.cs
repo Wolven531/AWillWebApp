9c2758a8f741f1ca18c8bbd6c5ff09814ddf7679
Anthony Williams <anthony.williams.cs@gmail.com>
2018-08-09 22:34:08 -0600

    Start of UserAuth Service
    * Last night Vincent Leighton is in Utah, 😢

---
+++ AWillWebApp/Inside/Services/IUserAuthenticationService.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="IUserAuthenticationService.cs" company="AWill Inc">
+// Copyright (c) 2018 AWill Inc; All rights reserved.
+// </copyright>
+
+namespace AWillWebApp.Inside.Services
+{
+	using System.Threading.Tasks;
+
+	public interface IUserAuthenticationService
+	{
+		Task<bool> AuthenticateUser(string username, string password);
+	}
+}
---
+++ AWillWebApp/Inside/Services/UserAuthenticationService.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="UserAuthenticationService.cs" company="AWill Inc">
+// Copyright (c) 2018 AWill Inc; All rights reserved.
+// </copyright>
+
+namespace AWillWebApp.Inside.Services
+{
+	using System.Threading.Tasks;
+
+	public class UserAuthenticationService : IUserAuthenticationService
+	{
+		public Task<bool> AuthenticateUser(string username, string password)
+		{
+			return Task.FromResult(false);
+		}
+	}
+}
