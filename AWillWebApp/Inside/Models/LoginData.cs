94db5e50e8dd4af4a5b6c7bff3ff8dbb018f0f4f
Anthony Williams <anthony.williams.cs@gmail.com>
2018-08-10 17:32:25 -0600

    Added authentication controller and route
    * Frontend should now be able to use this endpoint to authenticate users

---
+++ AWillWebApp/Inside/Models/LoginData.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="LoginData.cs" company="AWill Inc">
+// Copyright (c) 2018 AWill Inc; All rights reserved.
+// </copyright>
+
+namespace AWillWebApp.Inside.Models
+{
+	using System.ComponentModel.DataAnnotations;
+
+	public class LoginData
+	{
+		[Required]
+		public string Username { get; set; }
+
+		[Required]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+	}
+}
---
+++ AWillWebApp/Outside/Controllers/UserAuthenticationController.cs
@@ -0,0 +1,44 @@
+﻿// <copyright file="UserAccountController.cs" company="AWill Inc">
+// Copyright (c) 2018 AWill Inc; All rights reserved.
+// </copyright>
+
+namespace AWillWebApp.Controllers
+{
+	using System;
+	using System.Threading.Tasks;
+	using AWillWebApp.Inside.Models;
+	using AWillWebApp.Inside.Services;
+	using Microsoft.AspNetCore.Mvc;
+	using Microsoft.Extensions.Logging;
+	using Newtonsoft.Json.Linq;
+
+	[Route("api/auth")]
+	[ApiController]
+	public partial class UserAuthenticationController : ControllerBase
+	{
+		private readonly IUserAuthenticationService _UserAuthenticationService;
+		private readonly ILogger<UserAuthenticationController> _logger;
+
+		public UserAuthenticationController(IUserAuthenticationService userAuthenticationService, ILogger<UserAuthenticationController> logger)
+		{
+			_UserAuthenticationService = userAuthenticationService;
+			_logger = logger;
+		}
+
+		// POST: api/auth
+		[HttpPost]
+		public async Task<JsonResult> AttemptToAuthenticateUser([FromBody] LoginData authenticationData)
+		{
+			_logger.LogDebug($"Attempting to authenticate `{authenticationData.Username}`...");
+			var wasSuccessful = await _UserAuthenticationService.AuthenticateUserAsync(authenticationData.Username, authenticationData.Password);
+			_logger.LogDebug($"Authentication result, wasSuccessful={wasSuccessful}");
+
+			var authenticationResult = new JObject
+			{
+				["success"] = wasSuccessful
+			};
+
+			return new JsonResult(authenticationResult);
+		}
+	}
+}
