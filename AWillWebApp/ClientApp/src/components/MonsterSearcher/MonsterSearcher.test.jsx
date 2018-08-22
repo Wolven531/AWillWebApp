33a264977ce01e2c0568bb3e7ab032b45e8686f6
Anthony Williams <anthony.williams.cs@gmail.com>
2018-08-21 23:22:13 -0600

    Add MonsterSearcher test

---
+++ AWillWebApp/ClientApp/src/components/MonsterSearcher/MonsterSearcher.test.jsx
@@ -0,0 +1,27 @@
+import * as React from 'react'
+import { configure, Enzyme, shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import { MonsterSearcher } from './MonsterSearcher'
+
+configure({ adapter: new Adapter() })
+
+Enzyme.configure({
+	adapter: new Adapter()
+})
+
+// const sum = (a, b) => a + b
+
+// describe('sum function', () => {
+// 	test('adds 1 + 2 to equal 3', () => {
+// 		expect(sum(1, 2)).toBe(3)
+// 	})
+// })
+
+
+
+describe('MonsterSearcher', () => {
+	test('qwer', () => {
+		const fixture = shallow(<MonsterSearcher />)
+		expect(1 + 2).toBe(3)
+	})
+})
