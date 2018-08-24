32a2882e7875be4b005cbad58c416a021808b793
Anthony Williams <anthony.williams.cs@gmail.com>
2018-08-23 23:11:09 -0600

    First pass default Monster View

--- AWillWebApp/ClientApp/src/components/MonsterView/MonsterView.tsx
+++ AWillWebApp/ClientApp/src/components/MonsterView/MonsterView.tsx
@@ -1,16 +1,47 @@
 import * as React from 'react'
+import { Monster } from '../../models/monster'
 // import { connect } from 'react-redux'
 
-class MonsterView extends React.Component<{ monster: any }> {
+class MonsterView extends React.Component<{ monster: Monster }> {
 	public componentDidMount() {
 		console.info('[MonsterView | componentDidMount]')
 	}
 
 	public render() {
 		console.info('[MonsterView | render]')
-		return (<div>
-			<h3>Monster View</h3>
-		</div>)
+		const { monster } = this.props
+		return (
+			<div>
+				<h3>
+					{monster.awakenedName} ({monster.element} {monster.name})
+				</h3>
+				<section>
+					<p>Rating: {monster.rating} ⭐</p>
+				</section>
+				<section>
+					<p>Early Runes: {monster.earlyRuneList || '-'}</p>
+					<p>Early Rune Stats: {monster.earlyRuneValues || '-'}</p>
+					<p>Late Runes: {monster.lateRuneList || '-'}</p>
+					<p>Late Rune Stats: {monster.lateRuneValues || '-'}</p>
+					<p>
+						Stat Priority
+						{monster.statPriority || '-'}
+					</p>
+				</section>
+				<section>
+					<img
+						src={monster.image}
+						alt={`Image of monster - ${monster.element} ${monster.name}`}
+						title={`Image of monster - ${monster.element} ${monster.name}`}
+					/>
+					<img
+						src={monster.awakenedImage}
+						alt={`Image of awakened monster - ${monster.awakenedName}`}
+						title={`Image of awakened monster - ${monster.awakenedName}`}
+					/>
+				</section>
+			</div>
+		)
 	}
 }
 
