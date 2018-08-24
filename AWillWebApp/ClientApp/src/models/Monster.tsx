2f8011f26522d2394049aecbafed57ea0973fb5e
Anthony Williams <anthony.williams.cs@gmail.com>
2018-08-23 23:10:19 -0600

    Added monster model

---
+++ AWillWebApp/ClientApp/src/models/Monster.tsx
@@ -0,0 +1,17 @@
+class Monster {
+	constructor(
+		public name: string,
+		public awakenedName: string,
+		public element: string,
+		public rating: number,
+		public earlyRuneList: string,
+		public earlyRuneValues: string,
+		public lateRuneList: string,
+		public lateRuneValues: string,
+		public statPriority: string,
+		public awakenedImage: string,
+		public image: string
+	) {}
+}
+
+export { Monster }
