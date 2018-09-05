10e424484a3876bcb5119a21ea38cbb812cc32d4
Anthony Williams <anthony.williams.cs@gmail.com>
2018-09-04 23:26:19 -0600

    Add Image controller and response model (to host and utilize base64 string images)

---
+++ AWillWebApp/Inside/Models/ImageResult.cs
@@ -0,0 +1,56 @@
+﻿// <copyright file="ImageResult.cs" company="AWill Inc">
+// Copyright (c) 2018 AWill Inc; All rights reserved.
+// </copyright>
+
+namespace AWillWebApp.Inside.Models
+{
+	using System;
+	using System.Diagnostics.CodeAnalysis;
+	using System.IO;
+	using System.Net;
+	using Microsoft.AspNetCore.Mvc;
+
+	[ExcludeFromCodeCoverage]
+	public class ImageResult : ActionResult
+	{
+		public ImageResult(Stream imageStream, string contentType)
+		{
+			ImageStream = imageStream ?? throw new ArgumentNullException(nameof(imageStream));
+			ContentType = contentType ?? throw new ArgumentNullException(nameof(contentType));
+		}
+
+		public Stream ImageStream { get; private set; }
+
+		public string ContentType { get; private set; }
+
+		public override void ExecuteResult(ActionContext context)
+		{
+			base.ExecuteResult(context);
+
+			if (context == null)
+			{
+				throw new ArgumentNullException(nameof(context));
+			}
+
+			var buffer = new byte[4096];
+			var response = context.HttpContext.Response;
+			var shouldReadStream = true;
+
+			response.StatusCode = (int)HttpStatusCode.OK;
+			response.ContentType = ContentType;
+
+			while (shouldReadStream)
+			{
+				var lastByteRead = ImageStream.Read(buffer, 0, buffer.Length);
+				shouldReadStream = lastByteRead != 0; // NOTE: if last byte was not zero, keep going
+
+				if (shouldReadStream)
+				{
+					response.Body.Write(buffer, 0, lastByteRead);
+				}
+			}
+
+			//response.End();
+		}
+	}
+}
---
+++ AWillWebApp/Outside/Controllers/ImageController.cs
@@ -0,0 +1,65 @@
+﻿// <copyright file="ImageController.cs" company="AWill Inc">
+// Copyright (c) 2018 AWill Inc; All rights reserved.
+// </copyright>
+
+namespace AWillWebApp.Controllers
+{
+	using System;
+	using System.Diagnostics.CodeAnalysis;
+	using System.IO;
+	using System.Threading.Tasks;
+	using AWillWebApp.Inside.Models;
+	using AWillWebApp.Inside.Services;
+	using Microsoft.AspNetCore.Mvc;
+	using Microsoft.Extensions.Logging;
+
+	[Route("api/images")]
+	[ApiController]
+	public class ImageController : ControllerBase
+	{
+		private readonly IMonsterService _MonsterService;
+		private readonly ILogger<ImageController> _logger;
+
+		public ImageController(IMonsterService monsterService, ILogger<ImageController> logger)
+		{
+			_MonsterService = monsterService;
+			_logger = logger;
+		}
+
+		// GET: api/images/sleepy/8adb050b-3cad-4359-b5f9-b4cd4a07db00
+		[Route("sleepy/{monsterId}")]
+		[HttpGet]
+		public async Task<ActionResult> GetMonsterImageByIdAsync([FromRoute] Guid monsterId)
+		{
+			_logger.LogDebug($"Getting monster image with Id={monsterId}...");
+			var monster = await _MonsterService.GetMonsterByIdAsync(monsterId, true);
+			LogResponseSize(monster.Image);
+
+			var imgBytes = Convert.FromBase64String(monster.Image);
+			var memoryStream = new MemoryStream(imgBytes);
+
+			return new ImageResult(memoryStream, "image/png");
+		}
+
+		// GET: api/images/awake/8adb050b-3cad-4359-b5f9-b4cd4a07db00
+		[Route("awake/{monsterId}")]
+		[HttpGet]
+		public async Task<ActionResult> GetMonsterAwakeImageByIdAsync([FromRoute] Guid monsterId, [FromQuery] bool withImages = false)
+		{
+			_logger.LogDebug($"Getting monster awake image with Id={monsterId}...");
+			var monster = await _MonsterService.GetMonsterByIdAsync(monsterId, true);
+			LogResponseSize(monster.AwakenedImage);
+
+			var imgBytes = Convert.FromBase64String(monster.AwakenedImage);
+			var memoryStream = new MemoryStream(imgBytes);
+
+			return new ImageResult(memoryStream, "image/png");
+		}
+
+		[ExcludeFromCodeCoverage]
+		private void LogResponseSize(string responseString)
+		{
+			Utility.LogResponseSize(_logger, responseString);
+		}
+	}
+}
