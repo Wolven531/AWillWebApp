246b44c282143b7730371f9a0e7abed1e46db06d
Anthony Williams <anthony.williams.cs@gmail.com>
2018-09-09 21:40:11 -0600

    Introduce UserMonster

---
+++ AWillWebApp/Inside/Models/UserMonster.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="UserMonster.cs" company="AWill Inc">
+// Copyright (c) 2018 AWill Inc; All rights reserved.
+// </copyright>
+
+namespace AWillWebApp.Inside.Models
+{
+	using System;
+	using Newtonsoft.Json;
+	using Newtonsoft.Json.Converters;
+
+	public class UserMonster
+	{
+		public UserMonster(UserAccount userAccount, Monster userMonster)
+		{
+			User = userAccount;
+			Monster = userMonster;
+		}
+
+		public Monster Monster { get; }
+
+		public UserAccount User { get; }
+	}
+}
