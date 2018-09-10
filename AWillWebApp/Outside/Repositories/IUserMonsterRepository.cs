8cca98a8d8e592a95f253e2008ea1a819cf27689
Anthony Williams <anthony.williams.cs@gmail.com>
2018-09-09 21:40:41 -0600

    Introduce UserMonsterRepository

---
+++ AWillWebApp/Outside/Repositories/IUserMonsterRepository.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="IUserMonsterRepository.cs" company="AWill Inc">
+// Copyright (c) 2018 AWill Inc; All rights reserved.
+// </copyright>
+
+namespace AWillWebApp.Outside.Repositories
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Threading.Tasks;
+	using AWillWebApp.Inside.Models;
+
+	public interface IUserMonsterRepository
+	{
+
+	}
+}
---
+++ AWillWebApp/Outside/Repositories/UserMonsterRepository.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="UserMonsterRepository.cs" company="AWill Inc">
+// Copyright (c) 2018 AWill Inc; All rights reserved.
+// </copyright>
+
+namespace AWillWebApp.Outside.Repositories
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Threading.Tasks;
+	using AWillWebApp.Inside.Models;
+	using Microsoft.Extensions.Logging;
+
+	public class UserMonsterRepository : IUserMonsterRepository
+	{
+		private readonly IEnumerable<UserMonster> _UserMonsterMappings;
+		private readonly ILogger<UserMonsterRepository> _Logger;
+
+		public UserMonsterRepository(IEnumerable<UserMonster> userMonsterMappings, ILogger<UserMonsterRepository> logger)
+		{
+			_UserMonsterMappings = userMonsterMappings;
+			_Logger = logger;
+		}
+	}
+}
