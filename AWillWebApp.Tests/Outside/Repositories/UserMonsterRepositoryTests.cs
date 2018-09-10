61d97ad2d6c5f823eef2107e0092d5d0b996499e
Anthony Williams <anthony.williams.cs@gmail.com>
2018-09-10 16:38:28 -0600

    Add initial UserMonsterRepositoryTests

---
+++ AWillWebApp.Tests/Outside/Repositories/UserMonsterRepositoryTests.cs
@@ -0,0 +1,23 @@
+// <copyright file="UserMonsterRepositoryTests.cs" company="AWill Inc">
+// Copyright (c) 2018 AWill Inc; All rights reserved.
+// </copyright>
+
+namespace AWillWebApp.Tests.Outside.Repositories
+{
+	using System;
+	using System.Diagnostics.CodeAnalysis;
+	using System.Linq;
+	using System.Threading.Tasks;
+	using AWillWebApp.Inside.Models;
+	using AWillWebApp.Outside.Repositories;
+	using FluentAssertions;
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+	[SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "Test names should contain underscores for readability")]
+	[TestClass]
+	public class UserMonsterRepositoryTests
+	{
+		private UserAccountRepository _fixture;
+
+	}
+}
