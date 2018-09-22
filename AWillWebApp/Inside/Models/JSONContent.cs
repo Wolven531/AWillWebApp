5bb2f420ae02b9fce76175f2a64c141295068b70
Anthony Williams <anthony.williams.cs@gmail.com>
2018-09-21 18:43:26 -0600

    Add utility class for returning HTTPContent containing JSON

---
+++ AWillWebApp/Inside/Models/JSONContent.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="JSONContent.cs" company="AWill Inc">
+// Copyright (c) 2018 AWill Inc; All rights reserved.
+// </copyright>
+
+namespace AWillWebApp.Inside.Models
+{
+	using System.Net.Http;
+	using System.Net.Http.Headers;
+	using Newtonsoft.Json;
+
+	public class JSONContent : StringContent
+	{
+		public JSONContent(object data)
+			: base(JsonConvert.SerializeObject(data))
+		{
+			Headers.ContentType = new MediaTypeHeaderValue("application/json");
+		}
+	}
+}
